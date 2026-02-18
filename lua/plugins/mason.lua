7a7476b90b2f7330286fcda92f5b8aba6fab8c9f
francesco.dibella@thelevelgroup.com <francesco.dibella@thelevelgroup.com>
2026-02-18 14:55:29 +0100

    ensure installed python pkgs

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,11 @@
+-- Crea o modifica il file lua/plugins/mason.lua
+return {
+  "mason-org/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "basedpyright", -- LSP per Python
+      "ruff", -- Formatter/Linter
+      "debugpy", -- Debugger
+    },
+  },
+}

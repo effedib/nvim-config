bb2b8f162c24c0a7d41ab09e02f3813caab81420
francesco.dibella@thelevelgroup.com <francesco.dibella@thelevelgroup.com>
2026-03-02 17:38:22 +0100

    vim global variable

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,3 +1,17 @@
+-- Source - https://stackoverflow.com/a/79656109
+-- Posted by Jo Totland
+-- Retrieved 2026-03-02, License - CC BY-SA 4.0
+
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
+})
+
 -- Crea o modifica il file lua/plugins/mason.lua
 return {
   "mason-org/mason.nvim",

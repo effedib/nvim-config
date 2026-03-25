ea42160895ee4bdd5331d0230405eda16a5c1908
francesco.dibella@thelevelgroup.com <francesco.dibella@thelevelgroup.com>
2026-03-25 11:01:07 +0100

    revert transparent_background

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -23,38 +23,39 @@ return {
     opts = {},
   },
 
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      transparent_background = true, -- Questo buca lo sfondo principale
-      integrations = {
-        mini = true,
-        snacks = true, -- Rende trasparenti i dashboard e i popup di Snacks
-        telescope = {
-          enabled = true,
-          style = "nvchad", -- Stile pulito per le finestre di ricerca
-        },
-        neotree = true, -- Trasparenza per l'albero dei file
-        mason = true, -- Trasparenza per il menu di Mason
-        which_key = true,
-        render_markdown = true,
-      },
-      -- Questa funzione forza la trasparenza su elementi che a volte resistono
-      custom_highlights = function(colors)
-        return {
-          -- NormalFloat gestisce i popup (come le info di LSP o le finestre Snacks)
-          NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none", fg = colors.blue },
-          -- CursorLine rende la riga dove ti trovi leggermente visibile ma trasparente
-          CursorLine = { bg = colors.surface0 },
-          -- Pmenu è il menu del completamento automatico
-          Pmenu = { bg = "none" },
-          PmenuSel = { bg = colors.surface1, fg = "none" },
-        }
-      end,
-    },
-  },
+  -- transparent_background
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   opts = {
+  --     transparent_background = true, -- Questo buca lo sfondo principale
+  --     integrations = {
+  --       mini = true,
+  --       snacks = true, -- Rende trasparenti i dashboard e i popup di Snacks
+  --       telescope = {
+  --         enabled = true,
+  --         style = "nvchad", -- Stile pulito per le finestre di ricerca
+  --       },
+  --       neotree = true, -- Trasparenza per l'albero dei file
+  --       mason = true, -- Trasparenza per il menu di Mason
+  --       which_key = true,
+  --       render_markdown = true,
+  --     },
+  --     -- Questa funzione forza la trasparenza su elementi che a volte resistono
+  --     custom_highlights = function(colors)
+  --       return {
+  --         -- NormalFloat gestisce i popup (come le info di LSP o le finestre Snacks)
+  --         NormalFloat = { bg = "none" },
+  --         FloatBorder = { bg = "none", fg = colors.blue },
+  --         -- CursorLine rende la riga dove ti trovi leggermente visibile ma trasparente
+  --         CursorLine = { bg = colors.surface0 },
+  --         -- Pmenu è il menu del completamento automatico
+  --         Pmenu = { bg = "none" },
+  --         PmenuSel = { bg = colors.surface1, fg = "none" },
+  --       }
+  --     end,
+  --   },
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
